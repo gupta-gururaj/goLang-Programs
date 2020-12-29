a39f65f504e40dc0a9325be0d3998040d496a039
gupta-gururaj <76657561+gupta-gururaj@users.noreply.github.com>
2020-12-29 20:14:38 +0530

    Update sudoku.go

--- sudoku.go
+++ sudoku.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func printMatrix(matrix [9][9]int, i1, j1, i2, j2 int) {
+	for i := i1; i <= i2; i++ {
+		for j := j1; j <= j2; j++ {
+			fmt.Print(matrix[i][j], " ")
+		}
+		fmt.Println()
+	}
+}
+
 func sudokuRowColumnCheck(sudoku [9][9]int) int {
 	flag := 0
 
@@ -15,6 +24,10 @@ func sudokuRowColumnCheck(sudoku [9][9]int) int {
 			if element == 0 { //0 means "null" so skipping the element
 				continue
 			} else {
+				if element > 9 {
+					flag = 1
+					fmt.Println("ERROR - element more than 9\nrow - ", i+1, "\ncolumn - ", j+1)
+				}
 				//row check
 				for r := 0; r < 9 && flag != 1; r++ {
 					if r == i { //element should not be compared with itself so skip the part
@@ -46,37 +59,38 @@ func sudokuRowColumnCheck(sudoku [9][9]int) int {
 	return flag
 }
 
-func boxCheck(sudoku [9][9]int, si, sj, ei, ej int) int {
+func smallSudokuCheck(sudoku [9][9]int, iStart, jStart, iEnd, jEnd int) int {
 	var smallSudoku [9][9]int
-	for i := si; i <= ej; i++ {
-		for j := sj; j <= ej; j++ {
+	for i := iStart; i <= iEnd; i++ {
+		for j := jStart; j <= jEnd; j++ {
 			smallSudoku[i][j] = sudoku[i][j]
 		}
 	}
 
 	//To print small sudoku
-	/* for i := si; i <= ei; i++ {
-		for j := sj; j <= ej; j++ {
+	/* for i := iStart; i <= iEnd; i++ {
+		for j := jStart; j <= jEnd; j++ {
 			fmt.Print(smallSudoku[i][j], " ")
 		}
 		fmt.Println()
 	} */
 
 	var flag, count = 0, 0
-	for i := si; i <= ei && flag != 1; i++ {
-		for j := sj; j <= ej && flag != 1; j++ {
+	for i := iStart; i <= iEnd && flag != 1; i++ {
+		for j := jStart; j <= jEnd && flag != 1; j++ {
 			element := smallSudoku[i][j]
 			count = 0
 			if element == 0 { //0 means "null" so skipping the element
 				continue
 			} else {
-				for di := si; di <= ei && flag != 1; di++ {
-					for dj := sj; dj <= ej && flag != 1; dj++ {
+				for di := iStart; di <= iEnd && flag != 1; di++ {
+					for dj := jStart; dj <= jEnd && flag != 1; dj++ {
 						if element == smallSudoku[di][dj] && flag != 1 {
 							count++
 							if count == 2 {
 								flag = 1
-								fmt.Println("Element -", element, "\nrow - ", di+1, "\ncolumn - ", dj+1)
+								fmt.Println("Number -", element, "\nRow -", di+1, "\nColumn -", dj+1)
+								printMatrix(smallSudoku, iStart, jStart, iEnd, jEnd)
 								break
 							}
 						}
@@ -96,28 +110,23 @@ func main() {
 		{8, 0, 0, 0, 6, 0, 0, 0, 3},
 		{4, 0, 0, 8, 0, 3, 0, 0, 1},
 		{7, 0, 0, 0, 2, 0, 0, 0, 6},
-		{0, 6, 0, 0, 0, 0, 2, 8, 0},
+		{0, 6, 0, 0, 0, 0, 7, 8, 0},
 		{0, 0, 0, 4, 1, 9, 0, 0, 5},
-		{0, 0, 0, 0, 8, 0, 0, 7, 9},
+		{0, 0, 0, 0, 8, 0, 0, 2, 9},
 	}
 
-	//Print Sudoku
-	/* for i := 0; i < 9; i++ {
-		for j := 0; j < 9; j++ {
-			fmt.Print(sudoku[i][j], " ")
-		}
-		fmt.Println()
-	} */
+	/* printMatrix(sudoku, 0, 0, 8, 8)
+	fmt.Println("") */
 
-	v1 := boxCheck(sudoku, 0, 0, 2, 2)
-	v2 := boxCheck(sudoku, 3, 0, 5, 2)
-	v3 := boxCheck(sudoku, 6, 0, 8, 2)
-	v4 := boxCheck(sudoku, 0, 3, 2, 5)
-	v5 := boxCheck(sudoku, 3, 3, 5, 5)
-	v6 := boxCheck(sudoku, 6, 3, 8, 5)
-	v7 := boxCheck(sudoku, 0, 6, 2, 8)
-	v8 := boxCheck(sudoku, 3, 6, 5, 8)
-	v9 := boxCheck(sudoku, 6, 6, 8, 8)
+	v1 := smallSudokuCheck(sudoku, 0, 0, 2, 2)
+	v2 := smallSudokuCheck(sudoku, 3, 0, 5, 2)
+	v3 := smallSudokuCheck(sudoku, 6, 0, 8, 2)
+	v4 := smallSudokuCheck(sudoku, 0, 3, 2, 5)
+	v5 := smallSudokuCheck(sudoku, 3, 3, 5, 5)
+	v6 := smallSudokuCheck(sudoku, 6, 3, 8, 5)
+	v7 := smallSudokuCheck(sudoku, 0, 6, 2, 8)
+	v8 := smallSudokuCheck(sudoku, 3, 6, 5, 8)
+	v9 := smallSudokuCheck(sudoku, 6, 6, 8, 8)
 	sudokuValue := v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 //If anyone of them is false then value will be > 0
 
 	if sudokuValue == 0 && sudokuRowColumnCheck(sudoku) == 0 {
