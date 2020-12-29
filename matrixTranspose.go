046e8ca79700330dc54dc5d792bdbd238943b58b
gupta-gururaj <76657561+gupta-gururaj@users.noreply.github.com>
2020-12-29 20:05:04 +0530

    Update matrixTranspose.go

--- matrixTranspose.go
+++ matrixTranspose.go
@@ -18,7 +18,7 @@ func main() {
 		}
 	}
 
-	//taking transposed of the Matrix
+	//taking transpose of the Matrix
 	var transposedMatrix [10][10]int
 	for i := 0; i < row; i++ {
 		for j := 0; j < column; j++ {
